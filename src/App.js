916b8390c31ad0ede6efd33279364527a31dd4d9
Kashish Bansal <Kashishbansal081@gmail.com>
2025-08-05 11:36:34 +0530

    Frontend: Adding book library frontend code

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import AddNewBookForm from "./components/AddNewBookForm";
+import BookByAuthor from "./components/BookByAuthor";
+import BookByTitle from "./components/BookByTitle";
+import Books from "./components/Books";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <AddNewBookForm/>
+      <Books />
+      <BookByTitle title="To Kill a Mockingbird"/>
+      <BookByAuthor authorName="Harper Lee"/>
+    </div>
+  );
+}
+
+export default App;
